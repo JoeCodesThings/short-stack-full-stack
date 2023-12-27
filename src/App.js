e715bf3434b2fc8c0e63dbf8abe0d4218bf6d1a5
JoeCodesThings <joecodesthings@gmail.com>
2023-12-27 14:01:23 +0800

    Text test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> for a heckin' good time yo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How To Adulting Real Good
         </a>
       </header>
     </div>
